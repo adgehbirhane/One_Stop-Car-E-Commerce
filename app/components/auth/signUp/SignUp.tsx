087d64259a98cce354d54f9d9677453aa69f6954
Bly <adgehbirhane@gmail.com>
2024-04-06 19:57:18 +0300

    notice current update

--- app/components/auth/signUp/SignUp.tsx
+++ app/components/auth/signUp/SignUp.tsx
@@ -5,7 +5,7 @@ import axiosInstance from "@/app/api";
 import SERVER_API_URL from "@/app/config";
 import { User } from "@/app/types";
 import { jwtDecode } from "jwt-decode";
-import { FaGoogle } from "react-icons/fa";
+// import { FaGoogle } from "react-icons/fa";
 
 interface SignUpProps {
     onClose: () => void;
@@ -105,13 +105,15 @@ const SignUp: React.FC<SignUpProps> = ({ onClose, setUserLoggedIn, setCurrentPag
                     {loading && <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-gray-900"></div>}
                     Sign Up
                 </button>
-                <div className="m-1 text-center">Or</div>
-                <button
-                    onClick={() => setCurrentPage("signUpWithGoogle")}
-                    className=" flex flex-row justifyCenter gap-3 w-full bg-gray-200 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded"
-                >
-                    <FaGoogle /> Sign Up with Google
-                </button>
+                {/* 
+                    <div className="m-1 text-center">Or</div>
+                    <button
+                        onClick={() => setCurrentPage("signUpWithGoogle")}
+                        className=" flex flex-row justifyCenter gap-3 w-full bg-gray-200 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded"
+                    >
+                        <FaGoogle /> Sign Up with Google
+                    </button>
+                 */}
             </form>
         </div>
     );
